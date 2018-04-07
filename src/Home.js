0ea6360d152bd44c2ef500378b2b63215039a6e6
Kaley Main <kaleypoole17@gmail.com>
2018-04-07 13:44:28 +1000

    Add circleci config

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,18 @@ import "./Home.scss";
 
 import { createFetchPostsAction } from "./reducer";
 
-const noop = () => {};
+import Spinner from "./Spinner/Spinner";
 
 class Home extends Component {
   componentDidMount() {
-    this.props.store.dispatch(createFetchPostsAction());
+    if (!this.props.postsLoaded) {
+      this.props.store.dispatch(createFetchPostsAction());
+    }
   }
 
   render() {
     const posts = this.props.posts;
-    
+
     return (
       <div className="home">
         <h1>Kaley Main</h1>
@@ -24,6 +26,8 @@ class Home extends Component {
         <h3>Blog posts</h3>
 
         <div className="blog-links">
+          {this.props.loading ? <Spinner /> : null}
+
           {posts.map(post => (
             <Link key={post.slug} to={"/post/" + post.slug}>
               {post.title}
@@ -38,7 +42,7 @@ class Home extends Component {
 const mapStateToProps = state => ({
   posts: state.posts,
   loading: state.loading,
+  postsLoaded: state.postsLoaded
 });
 
 export default connect(mapStateToProps)(Home);
-
